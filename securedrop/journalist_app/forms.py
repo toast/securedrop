7a8558119e105d8579121cfc707d87dadf307ad3
redshiftzero <jen@freedom.press>
2017-11-01 12:07:02 -0700

    Journalist interface: Add NewUserForm
    Validate form inputs using WTForms. This fixes #2500, where an odd
    length input would throw a 500 error.

--- securedrop/journalist_app/forms.py
+++ securedrop/journalist_app/forms.py
@@ -2,8 +2,26 @@
 
 from flask_babel import gettext
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField
-from wtforms.validators import InputRequired
+from wtforms import TextAreaField, TextField, BooleanField, HiddenField
+from wtforms.validators import InputRequired, Optional, Length
+
+
+class NewUserForm(FlaskForm):
+     username = TextField('username', validators=[
+         InputRequired(message=gettext('This field is required.')),
+         Length(min=Journalist.MIN_USERNAME_LEN,
+                message=gettext('Field must be at least {min_chars} '
+                                'characters long.'.format(
+                                    min_chars=Journalist.MIN_USERNAME_LEN))),
+     ])
+     password = HiddenField('password')
+     is_admin = BooleanField('is_admin')
+     is_hotp = BooleanField('is_hotp')
+     otp_secret = TextField('otp_secret', validators=[
+         Length(min=40, max=40,
+                message=gettext('Field must be 40 characters long.')),
+         Optional()
+     ])
 
 
 class ReplyForm(FlaskForm):
